cb65e026f553823bd49992a74c6735f36bb3294a
RocketBunny <guzenkovsk@gmail.com>
2025-05-28 22:35:27 +0700

    init project

---
+++ src/main/java/com/example/mytelegramauthentication/MyTelegramAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytelegramauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTelegramAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTelegramAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mytelegramauthentication/MyTelegramAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytelegramauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTelegramAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
